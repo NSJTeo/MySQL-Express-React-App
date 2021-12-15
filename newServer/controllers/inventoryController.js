335df50df23737d2d25e1466979bb53840f19a7b
NSJTeo <n.teomacdonald@gmail.com>
2021-12-15 12:47:14 -0800

    Add inventoryController functions

--- newServer/controllers/inventoryController.js
+++ newServer/controllers/inventoryController.js
@@ -1,12 +1,62 @@
 const knex = require("knex")(require("../knexfile").development);
 
-exports.index = (_req, res) => {
+exports.getInventory = (_req, res) => {
   // knex("inventory") is similar to SELECT * FROM inventory (returns promise)
   knex("inventory")
     .then((data) => {
       res.status(200).json(data);
     })
+    .catch((err) => res.status(400).send(`Error retrieving Inventory: ${err}`));
+};
+
+exports.createInventoryItem = (req, res) => {
+  knex("inventory")
+    .insert(req.body)
+    .then((data) => {
+      res.status(200).json(data);
+    })
     .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories: ${err}`)
+      res.status(400).send(`Error creating Inventory Item: ${err}`)
+    );
+};
+
+exports.getItem = (req, res) => {
+  knex("inventory")
+    .where({ id: req.params.id })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error retrieving Inventory Item ${req.params.id} ${err}`)
+    );
+};
+
+exports.deleteItem = (req, res) => {
+  knex("inventory")
+    .delete()
+    .where({ id: req.params.id })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error deleting Inventory Item ${req.params.id} ${err}`)
+    );
+};
+
+exports.editItem = (req, res) => {
+  knex("inventory")
+    .update(req.body)
+    .where({ id: req.params.id })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error updating Inventory Item ${req.params.id} ${err}`)
     );
 };
