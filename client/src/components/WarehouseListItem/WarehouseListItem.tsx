2d0dd71675d187e8140150c1273d6790bc2f741d
NSJTeo <n.teomacdonald@gmail.com>
2021-12-16 13:20:06 -0800

    WarehouseListItem, WarehouseListSearchBar styles

--- client/src/components/WarehouseListItem/WarehouseListItem.tsx
+++ client/src/components/WarehouseListItem/WarehouseListItem.tsx
@@ -2,6 +2,9 @@ import React, { ReactElement } from "react";
 import { WarehouseProfile } from "../../types/types";
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import "./WarehouseListItem.scss";
+import nextIcon from "../../assets/icons/chevron_right-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
 
 interface Props {
   warehouse: WarehouseProfile;
@@ -17,12 +20,64 @@ export default function WarehouseListItem({
   };
 
   return (
-    <li>
-      <Link to={`/warehouse/${warehouse.id}`}>{warehouse.name}</Link>
-      <button onClick={handleClick}>
-        <img src={deleteIcon} alt="delete icon" />
-      </button>
-      <Link to={`/warehouse/${warehouse.id}/edit`}>Edit</Link>
+    <li className="warehouse-list-item__container">
+      <div className="warehouse-list-item__warehouse-contact-container">
+        <div className="warehouse-list-item__warehouse-container">
+          <h2 className="warehouse-list-item__warehouse-header">WAREHOUSE</h2>
+          <Link
+            to={`/warehouse/${warehouse.id}`}
+            className="warehouse-list-item__warehouse-link"
+          >
+            <p className="warehouse-list-item__warehouse-link-text">
+              {warehouse.name}
+            </p>
+            <img
+              src={nextIcon}
+              alt=""
+              className="warehouse-list-item__warehouse-link-icon"
+            />
+          </Link>
+        </div>
+        <div className="warehouse-list-item__contact-container">
+          <h2 className="warehouse-list-item__contact-header">CONTACT NAME</h2>
+          <p className="warehouse-list-item__contact">
+            {warehouse.contactName}
+          </p>
+        </div>
+      </div>
+      <div className="warehouse-list-item__address-contact-info-container">
+        <div className="warehouse-list-item__address-container">
+          <h2 className="warehouse-list-item__address-header">ADDRESS</h2>
+          <p className="warehouse-list-item__address">
+            {warehouse.address}, {warehouse.city}, {warehouse.country}
+          </p>
+        </div>
+        <div className="warehouse-list-item__contact-info-container">
+          <h2 className="warehouse-list-item__contact-info-header">
+            CONTACT INFORMATION
+          </h2>
+          <p className="warehouse-list-item__contact-info">
+            {warehouse.contactPhone}
+          </p>
+          <p className="warehouse-list-item__contact-info">
+            {warehouse.contactEmail}
+          </p>
+        </div>
+      </div>
+      <div className="warehouse-list-item__btns-container">
+        <button
+          onClick={handleClick}
+          className="warehouse-list-item__btn warehouse-list-item__btn--delete"
+        >
+          <img src={deleteIcon} alt="delete icon" />
+        </button>
+        <Link
+          to={`/warehouse/${warehouse.id}/edit`}
+          className="warehouse-list-item__btn warehouse-list-item__btn--edit"
+        >
+          <img src={editIcon} alt="" />
+        </Link>
+      </div>
     </li>
   );
 }
