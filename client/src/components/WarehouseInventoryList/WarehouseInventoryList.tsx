d5d652e8961eb1fe5440b95bc0df6b394e9baed2
NSJTeo <n.teomacdonald@gmail.com>
2021-12-27 16:43:56 -0800

    Refactor inventory list-specific styles into warehouse-inventory-list style file

--- client/src/components/WarehouseInventoryList/WarehouseInventoryList.tsx
+++ client/src/components/WarehouseInventoryList/WarehouseInventoryList.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import { InventoryItemInfo } from "../../types/types";
+import "./WarehouseInventoryList.scss";
 import WarehouseInventoryListItem from "../WarehouseInventoryListItem/WarehouseInventoryListItem";
+import sortIcon from "../../assets/icons/sort-24px.svg";
 
 interface Props {
   inventoryItems: InventoryItemInfo[];
@@ -12,16 +14,61 @@ export default function WarehouseInventoryList({
   handleDelete,
 }: Props): ReactElement {
   return (
-    <ul>
-      {inventoryItems.map((inventoryItem) => {
-        return (
-          <WarehouseInventoryListItem
-            key={inventoryItem.id}
-            inventoryItem={inventoryItem}
-            handleDelete={handleDelete}
-          />
-        );
-      })}
-    </ul>
+    <>
+      <div className="warehouse-inventory-list__headers">
+        <div className="warehouse-inventory-list__item-category-container">
+          <div className="warehouse-inventory-list__header-container">
+            <h2 className="warehouse-inventory-list__header">INVENTORY ITEM</h2>
+            <img
+              src={sortIcon}
+              alt=""
+              className="warehouse-inventory-list__sort-icon"
+            />
+          </div>
+          <div className="warehouse-inventory-list__header-container">
+            <h2 className="warehouse-inventory-list__header">CATEGORY</h2>
+            <img
+              src={sortIcon}
+              alt=""
+              className="warehouse-inventory-list__sort-icon"
+            />
+          </div>
+        </div>
+        <div className="warehouse-inventory-list__status-quantity-container">
+          <div className="warehouse-inventory-list__header-container">
+            <h2 className="warehouse-inventory-list__header">STATUS</h2>
+            <img
+              src={sortIcon}
+              alt=""
+              className="warehouse-inventory-list__sort-icon"
+            />
+          </div>
+          <div className="warehouse-inventory-list__header-container">
+            <h2 className="warehouse-inventory-list__header">QUANTITY</h2>
+            <img
+              src={sortIcon}
+              alt=""
+              className="warehouse-inventory-list__sort-icon"
+            />
+          </div>
+        </div>
+        <div className="warehouse-inventory-list__header-container warehouse-inventory-list__header-container--actions">
+          <h2 className="warehouse-inventory-list__header warehouse-inventory-list__header--actions">
+            ACTIONS
+          </h2>
+        </div>
+      </div>
+      <ul>
+        {inventoryItems.map((inventoryItem) => {
+          return (
+            <WarehouseInventoryListItem
+              key={inventoryItem.id}
+              inventoryItem={inventoryItem}
+              handleDelete={handleDelete}
+            />
+          );
+        })}
+      </ul>
+    </>
   );
 }
